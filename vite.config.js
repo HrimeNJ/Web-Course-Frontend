eb4b095764a395613bf66c17102614acd4d2eac6
Hrime <1970902125@qq.com>
2024-08-12 00:47:11 +0800

    Build and Github Actions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Web-Course-Frontend/',  // 设置为你的仓库名称
   plugins: [react()],
 })
